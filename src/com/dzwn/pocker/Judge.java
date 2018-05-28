c625b9276ff0843ce563d453f490025d18185bc7
Yuanxingqian <844800670@qq.com>
2018-05-28 19:38:27 +0800

    修改了一些方法
    将手牌列表改为了映射，现需要重新改为列表+映射

--- src/com/dzwn/pocker/Judge.java
+++ src/com/dzwn/pocker/Judge.java
@@ -1,16 +1,25 @@
 package com.dzwn.pocker;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Judge {
+	
+	/**
+	 * 裁判发牌程序
+	 * @param pc 初始化好的扑克牌
+	 * @param p1  玩家（人类）
+	 * @param p2  玩家（电脑）
+	 * @return String 返回字符串发牌完毕
+	 */
 	public String deal(PockerCards pc,Player p1,Player p2){
-		List<String> cards = pc.getCards();
+		Map<String,String> cards = pc.getCards();
 		for(int i=0;i<4;i++){
 			Random random = new Random();
 			int cardNumber = random.nextInt(cards.size()-1);
 			if(i%2==0){
-				p1.getHandCards().add(cards.get(cardNumber));
+				p1.getHandCards()(cards.get(cardNumber));
 				cards.remove(cardNumber);
 			}else{
 				p2.getHandCards().add(cards.get(cardNumber));
@@ -19,4 +28,43 @@ public class Judge {
 		}
 		return "发牌完毕！";
 	}
+	
+	public String compare(Player p1,Player p2){
+		String p1sCard1 = p1.getHandCards().get(0).substring(p1.getHandCards().get(0).length()-1, p1.getHandCards().get(0).length());
+		String p1sCard2 = p1.getHandCards().get(1).substring(p1.getHandCards().get(1).length()-1, p1.getHandCards().get(0).length());
+		String p2sCard1 = p2.getHandCards().get(0).substring(p2.getHandCards().get(0).length()-1, p2.getHandCards().get(0).length());
+		String p2sCard2 = p2.getHandCards().get(1).substring(p2.getHandCards().get(1).length()-1, p2.getHandCards().get(0).length());
+		
+		int p1sMaxCard = string2int(p1sCard1)>string2int(p1sCard2)?string2int(p1sCard1):string2int(p1sCard2);
+		int p2sMaxCard = string2int(p1sCard1)>string2int(p1sCard2)?string2int(p1sCard1):string2int(p1sCard2);
+		
+		return "A";
+	}
+	
+	/**
+	 * 将扑克牌点数由字母转换成数字
+	 * @param card 扑克牌的字母
+	 * @return 扑克牌点数对应的数字
+	 */
+	private int string2int(String card){
+		int cardsInt=0;
+		if(card.equals("A")){
+			cardsInt=1;
+		}else if(card.equals("J")){
+			cardsInt=11;
+		}else if(card.equals("Q")){
+			cardsInt=12;
+		}else if(card.equals("K")){
+			cardsInt=13;
+		}else if(card.equals("小王")){
+			cardsInt=14;
+		}else if(card.equals("大王")){
+			cardsInt=15;
+		}else{
+			cardsInt=Integer.parseInt(card);
+		}
+		
+		return cardsInt;
+	}
+	
 }
--- src/com/dzwn/pocker/PockerCards.java
+++ src/com/dzwn/pocker/PockerCards.java
@@ -1,25 +1,29 @@
 package com.dzwn.pocker;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PockerCards {
-	private List<String> cards;
+	private Map<String,String> cards;
 	
 	
-	public List<String> getCards() {
+	public Map<String,String> getCards() {
 		return cards;
 	}
 
 
-	public void setCards(List<String> cards) {
+	public void setCards(Map<String,String> cards) {
 		this.cards = cards;
 	}
-
-
+	
+	/**
+	 * 初始化扑克牌
+	 */
 	public PockerCards(){
 		System.out.println("初始化扑克牌......");
-		cards = new ArrayList<String>();
+		cards = new HashMap<String,String>();
 		for(int i=0;i<4;i++){
 			for(int j=0;j<13;j++){
 				String type="";
@@ -44,11 +48,13 @@ public class PockerCards {
 				}else if(j==12){
 					num="K";
 				}
-				cards.add(type+num);
+				Map<String,String> card = new HashMap<String,String>();
+				cards.put(type, num);
 			}
 		}
-		cards.add("大王");
-		cards.add("小王");
+		cards.put("小王", "小王");
+		cards.put("大王", "大王");
+		
 		System.out.println("扑克牌初始化完毕！");
 	}
 }
